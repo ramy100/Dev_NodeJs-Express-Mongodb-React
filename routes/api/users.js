f777882301d6a6d52fd12bd6a724f8dac8b471b5
ramy2020 <ramy.alashmawy@outlook.com>
2020-03-12 19:24:40 +0200

    added register and insert into database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 //@route        Post api/users
 //@desc         Register route
@@ -18,13 +21,44 @@ router.post(
       'Enter a valid password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user already exists' }] });
+      }
+      //get users gravatar
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //encrypt password
+      //if we dont use await we will need to user .then() and .catch() to handle the promise
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      //reuturn jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
