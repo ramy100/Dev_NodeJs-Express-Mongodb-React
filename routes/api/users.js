c917ec627f7a46551730d92ca6d536260784ae9d
ramy2020 <ramy.alashmawy@outlook.com>
2020-03-12 17:35:23 +0200

    added express validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-//@route        GET api/users
-//@desc         Test route
+//@route        Post api/users
+//@desc         Register route
 //@access       Public
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check(
+      'password',
+      'Enter a valid password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
